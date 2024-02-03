72ac0eeb90663e55bbadc3eb78496308565acf96
Hassan <muhammadhassanchannel786@gmail.com>
2024-02-03 13:08:10 +0500

    feat: codemirror setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
-import './App.css'
-import Test from './components/Test'
+import { useState, useEffect, useRef } from 'react'
+import CodeEditor from './components/codeEditors'
+import Navbar from './components/Navbar'
+import NewEditor from './components/NewEditor'
+// import CodeEditor from '@/components/codeEditor'
 
-function App() {
+
+export default function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+
+  const iframeRef = useRef(null);
+  const handleIframeError = (event: any) => {
+    // const errorMessage = event.message;
+    // console.error('Error in the iframe:', errorMessage);
+    console.log('hellow')
+    // Handle the error as needed
+  };
+
+  
+  useEffect(() => {
+
+      const iframe = iframeRef.current;
+      console.log(iframe)
+      iframe?.contentWindow?.addEventListener('error', handleIframeError);
+
+    // return () => {
+    //   iframe.contentWindow.removeEventListener('mouseup', handleIframeError);
+    // };
+  }, []);
+
 
   return (
-    <Test />
+    <div>
+      <div className='min-h-screen flex flex-col scrollbar scrollbar-thumb-red-600'>
+        <Navbar />
+
+        <main className='flex flex-col flex-grow'>
+
+          <div className='upper flex justify-between px-3 pt-1 pb-5 bg-black'>
+            <NewEditor language='xml' name='HTML' value={html} onChange={setHtml} />
+            <NewEditor language='css' name='CSS' value={css} onChange={setCss} />
+            <NewEditor language='javascript' name='JavaScript' value={js} onChange={setJs} />
+          </div>
+
+          <div className="lower h-[500px]">
+            <iframe
+              srcDoc={`<html>
+              <body style='margin-top: 200px;'>${html}</body>
+              <style>${css}</style>
+              <script>${js}</script>
+              </html>`} 
+              title='output' 
+              className='w-full h-full'
+              // sandbox='allow-scripts' //for security purposes (commented out in order to show console errors)
+              ref={iframeRef} />
+          </div>
+
+        </main> 
+      </div>
+    </div>
   )
 }
-
-export default App
---
+++ src/components/NewEditor.tsx
@@ -0,0 +1,52 @@
+
+import { Controlled as ControlledEditor } from "react-codemirror2";
+// import { Editor, EditorChange } from "codemirror";
+
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/base16-dark.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import 'codemirror/addon/edit/closebrackets'
+
+type NewEditorProps = {
+  language: string;
+  name: string;
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const NewEditor = ({
+  language,
+  name,
+  value,
+  onChange
+}: NewEditorProps) => {
+
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
+  return (
+    <div className='w-[33%] h-full flex flex-col items-start'>
+      <div className='bg-[#151515] text-gray-400 flex items-center gap-1 px-3 text-xs lg:text-md lg:font-semibold border-t-2 border-gray-400 py-2'><span className={`text-black text-xs w-5 h-5 flex items-center justify-center rounded-md ${name === 'HTML' ? 'bg-red-500' : name === 'CSS' ? 'bg-blue-500' : 'bg-yellow-400'}`}>{name === 'HTML' ? '/' : name === 'CSS' ? '#' : '()'}</span>{name}</div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code w-full text-sm flex-grow scrollbar scrollbar-thumb-pink-500"
+        style={{fontFamily: 'Source Code Pro'}}
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'base16-dark',
+          lineNumbers: true,
+          autoCloseBrackets: true
+        }}
+      />
+    </div>
+  );
+};
+
+export default NewEditor;
